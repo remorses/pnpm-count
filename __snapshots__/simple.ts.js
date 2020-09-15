69f1a90c5c63563908f7832b5e8a052f450251f4
remorses <beats.by.morse@gmail.com>
2020-09-15 15:02:43 +0200

    __snapshots__/simple.ts: snap

--- __snapshots__/simple.ts.js
+++ __snapshots__/simple.ts.js
@@ -1,8 +1,46 @@
-exports['processWhyOutput 1'] = [
-  {
-    "from": "lodash",
-    "version": "4.17.20",
-    "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
-    "topLevelDependent": "@pnpm/plugin-commands-server"
-  }
-]
+exports['processWhyOutput 1'] = {
+  "4.17.20": [
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "@pnpm/plugin-commands-server"
+    },
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "@pnpm/plugin-commands-store"
+    },
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "pnpm"
+    },
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "supi"
+    },
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "@pnpm/tarball-fetcher"
+    },
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "@pnpm-private/tools"
+    },
+    {
+      "from": "lodash",
+      "version": "4.17.20",
+      "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.17.20.tgz",
+      "topLevelDependent": "@pnpm/eslint-config"
+    }
+  ]
+}
